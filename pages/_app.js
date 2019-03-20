917aafcf3130e7816e2e4dc3f722c37683c34426
Детев Антон Сергеевич <Detev.A.Se@sberbank.ru>
2019-03-20 17:49:17 +0400

    Добавлено правило отступов, добавлены пустые строки в конце файлов

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,14 @@ import App, { Container } from 'next/app'
 import Layout from '../layouts/layout'
 
 export default class extends App {
-  render () {
-    const { Component, pageProps } = this.props
-    return (
-      <Container>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </Container>
-    )
-  }
+    render () {
+        const { Component, pageProps } = this.props
+        return (
+            <Container>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </Container>
+        )
+    }
 }
